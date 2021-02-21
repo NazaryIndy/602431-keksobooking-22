e52620d93dad6bb8c8818bce44191d4a2bdb91fe
Galina Pryadkina <galya@MacBook-Pro-Galina.local>
2021-02-21 18:19:37 +0300

    Fetch master

--- js/ad-form.js
+++ js/ad-form.js
@@ -11,6 +11,9 @@ const typeOfHousingElement = form.querySelector('#type');
 const priceElement = form.querySelector('#price');
 const timeInElement = form.querySelector('#timein');
 const timeOutElement = form.querySelector('#timeout');
+const roomNumber = form.querySelector('#room_number');
+const capacity = form.querySelector('#capacity');
+
 
 typeOfHousingElement.addEventListener('change', (evt) => {
   priceElement.placeholder = priceElement.min = MIN_PRICES[evt.target.value];
@@ -22,4 +25,47 @@ timeInElement.addEventListener('change', (evt) => {
 
 timeOutElement.addEventListener('change', (evt) => {
   timeInElement.value = evt.target.value;
+});
+
+
+const options = capacity.querySelectorAll('option');
+
+roomNumber.addEventListener('change', (evt) => {
+  if (evt.target.value === '1') {
+    capacity.value = evt.target.value;
+    for (let option of options) {
+      if (option.value !== '1') {
+        option.disabled = true;
+      } else {
+        option.disabled = false;
+      }
+    }
+  } else if (evt.target.value === '2') {
+    capacity.value = evt.target.value;
+    for (let option of options) {
+
+      if (option.value !== '1' && option.value !== '2') {
+        option.disabled = true;
+      } else {
+        option.disabled = false;
+      }
+    }
+  } else if (evt.target.value === '3') {
+    capacity.value = evt.target.value;
+    for (let option of options) {
+      if (option.value !== '1' && option.value !== '2' && option.value !== '3') {
+        option.disabled = true;
+      } else {
+        option.disabled = false;
+      }
+    }
+  } else if (evt.target.value === '100') {
+    capacity.value = '0';
+    for (let option of options) {
+      if (option.value !== '0') {
+        option.disabled = true;
+      }
+    }
+  }
+
 });
