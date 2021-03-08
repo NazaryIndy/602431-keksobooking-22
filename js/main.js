1d6aff96940601cf04dedc0e9bc543117a077acb
Galina Pryadkina <galya@MacBook-Pro-Galina.local>
2021-03-08 23:51:00 +0300

    Add debounce

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 import { setAdFormSubmit, setAdFormReset } from './ad-form.js';
 import { setFilters, mapFiltersForm } from './map-form.js';
 import { addOffersToMap, clearMarkers } from './map.js';
-import { showAlert, disableForm, enableForm } from './util.js';
+import { showAlert, disableForm, enableForm, debounce } from './util.js';
 import { getData } from './api.js';
 import { resetForm } from './reset-form.js';
+
+const RERENDER_DELAY = 500;
 
 getData(
   (offers) => {
     addOffersToMap(offers);
     enableForm(mapFiltersForm, 'map__filters');
-    setFilters(() => {
-      clearMarkers();
-      addOffersToMap(offers);
-    });
+    setFilters(debounce(
+      () => {
+        clearMarkers();
+        addOffersToMap(offers);
+      },
+      RERENDER_DELAY,
+    ));
   },
   (errorMessage) => {
     disableForm(mapFiltersForm, 'map__filters');
